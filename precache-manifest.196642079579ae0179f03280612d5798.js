95a5428e7f6c8c678ac5f6a0a56fa7f394c79ad6
shon5544 <65557784+shon5544@users.noreply.github.com>
2020-08-09 18:34:53 +0900

    Updates

---
+++ precache-manifest.196642079579ae0179f03280612d5798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64bdc485b5c3149aa5d94e622ca83ee4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "934f1b7e6a12b448cc57",
+    "url": "/static/css/main.b2b9954d.chunk.css"
+  },
+  {
+    "revision": "f6c95a6c92ef169d51e3",
+    "url": "/static/js/2.46580d50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.46580d50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "934f1b7e6a12b448cc57",
+    "url": "/static/js/main.fc55067f.chunk.js"
+  },
+  {
+    "revision": "488f91617323f85a90b8",
+    "url": "/static/js/runtime-main.0c959420.js"
+  }
+]);
